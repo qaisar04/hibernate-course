21813a6811cb75ca3d0a3f2110509fde6b878f1f
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-01-04 23:40:16 +0600

    read only mode

--- src/main/java/kz/baltabayev/HibernateRunner.java
+++ src/main/java/kz/baltabayev/HibernateRunner.java
@@ -8,28 +8,25 @@ import kz.baltabayev.util.TestDataImporter;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import java.util.List;
+
 public class HibernateRunner {
     @Transactional
     public static void main(String[] args) {
 
         try(SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-            Session session = sessionFactory.openSession();
-            Session session1 = sessionFactory.openSession()) {
-            TestDataImporter.importData(sessionFactory);
+            Session session = sessionFactory.openSession()) {
+//            TestDataImporter.importData(sessionFactory);
+            session.setDefaultReadOnly(true);
             session.beginTransaction();
-            session1.beginTransaction();
 
-            session.createQuery("select p from Payment p", Payment.class)
-                    .setLockMode(LockModeType.PESSIMISTIC_FORCE_INCREMENT)
+            Payment payment = session.find(Payment.class, 1L);
+            payment.setAmount(payment.getAmount() + 10); // данный update не будет действовать так как стоит read only мод
+
+            List<Payment> payments = session.createQuery("select p from Payment p", Payment.class)
+                    .setReadOnly(true)
                     .list();
 
-            Payment payment = session.find(Payment.class, 1L, LockModeType.PESSIMISTIC_FORCE_INCREMENT); // where p1_0.id=? for no key update nowait (and update version in table)
-            payment.setAmount(payment.getAmount() + 10);
-
-            Payment theSamePayment = session1.find(Payment.class, 1L);
-            theSamePayment.setAmount(theSamePayment.getAmount() + 15);
-
-            session1.getTransaction().commit();
             session.getTransaction().commit();
         }
     }
